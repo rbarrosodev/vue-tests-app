4e1e795c6d7a20342f15ee71361b0ce34963a53e
Rodrigo Barroso <digorbarroso@gmail.com>
2021-12-15 19:15:36 -0300

    Adds News updated

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="card mx-auto" style="width: 40rem;">
-        <ul class="list-group list-group-flush">
+    <div class="card mx-auto" style="width: 40rem; margin-top: 100px;">
+        <ul id="infinite-list" class="list-group list-group-flush">
             <li class="list-group-item" v-for="item in titles"><a :href="[item.url]" style="text-decoration: none;" target="_blank">{{ item.title }}</a></li>
         </ul>
     </div>
@@ -12,29 +12,25 @@
     @Options({})
     export default class News extends Vue {
         titles = [];
-
-        async beforeMount() {
-            const response = await this.axios.get(`https://newsapi.org/v2/top-headlines?country=br&category=general&apiKey=18e19938a9524d4ca24792af52f27251`);
-            for(var i in response.data.articles){
-                this.titles.push(response.data.articles[i]);
-            }
-        }
+        page = 1;
 
         async mounted(){
+             window.onscroll = () => {
+                let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+                if (bottomOfWindow) {
+                    this.page++;
+                    this.getNewNews();
+                }
+            };
             this.getNewNews();
         }
 
         getNewNews() {
-            window.onscroll = () => {
-                let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
-                if (bottomOfWindow) {
-                this.axios.get(`https://newsapi.org/v2/top-headlines?country=br&apiKey=18e19938a9524d4ca24792af52f27251`).then(response => {
-                        for(var i in response.data.articles){
-                            this.titles.push(response.data.articles[i]);
-                        }
-                    });
+            this.axios.get(`https://newsapi.org/v2/top-headlines?country=br&pagesize=5&page=${this.page}&apiKey=b180df5ee2984fbdafde4c5b1fe6958b`).then(response => {
+                for(var i in response.data.articles){
+                    this.titles.push(response.data.articles[i]);
                 }
-            }
+            });
         }
     }
 </script>
